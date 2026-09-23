8dadafbcab8620ab8b4c2d724961c6737ffc7017
yazmi-gif <yazmi@iespompeufabra.cat>
2026-09-23 14:30:20 +0200

    Update sketch.js

--- cara1/sketch.js
+++ cara1/sketch.js
@@ -1,1 +1,13 @@
+function setup() {
+  createCanvas(600, 600);
+}
 
+function draw() {
+  background(0,0,200);
+ellipse(300,200,300,400)
+ellipse(230,140,50,30)
+ellipse(230,140,30,30)
+ellipse(370,140,50,30)
+ellipse(370,140,30,30)
+arc(300,270,220,40,0,PI)
+triangle(300,200,300,200,350,270)}
